a14eb88da3efa9f925d46f98c85c2f777caf9b02
Akshat <your.actual.akshatkumar.xa@gmail.com>
2025-10-21 18:28:24 +0530

    feat: Add root page.tsx to redirect homepage to /sign-in

--- jsm_mock_interview_platform/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-
-import { redirect } from 'next/navigation';
-
-export default function HomeRedirect() {
-  // This tells the router that anyone hitting the root URL (/) 
-  // should immediately be sent to /sign-in.
-  redirect('/sign-in');
-}
